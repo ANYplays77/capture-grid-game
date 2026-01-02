f2e9049a0407988e1bddc5fe7488023c2c21d6ca
ANYplays77 <111249155+ANYplays77@users.noreply.github.com>
2026-01-02 19:56:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const io = new Server(server);
 app.use(express.static(__dirname));
 
 io.on('connection', (socket) => {
+    // Rejoindre un salon
     socket.on('joinRoom', (roomName) => {
         const room = io.sockets.adapter.rooms.get(roomName);
         const numClients = room ? room.size : 0;
@@ -22,8 +23,14 @@ io.on('connection', (socket) => {
         }
     });
 
+    // Transmettre un coup
     socket.on('makeMove', (data) => {
         socket.to(data.room).emit('moveMade', data);
+    });
+
+    // Transmettre un message de chat
+    socket.on('chatMessage', (data) => {
+        socket.to(data.room).emit('receiveMessage', data);
     });
 
     socket.on('gameOver', (data) => {
@@ -31,7 +38,5 @@ io.on('connection', (socket) => {
     });
 });
 
-
-// Remplace le port fixe 3000 par celui de Render
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
+server.listen(PORT, () => console.log(`Serveur prêt sur le port ${PORT}`));
